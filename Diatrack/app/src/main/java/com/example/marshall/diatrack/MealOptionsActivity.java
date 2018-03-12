0b50a599129ac8fe099346f3bfaa87369cb30a23
Evelyn Lima Rocha <elimarocha@student.fullsail.edu>
2018-03-11 20:47:13 -0400

    Adding List View

--- Diatrack/app/src/main/java/com/example/marshall/diatrack/MealOptionsActivity.java
+++ Diatrack/app/src/main/java/com/example/marshall/diatrack/MealOptionsActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -11,49 +12,63 @@ import android.widget.TextView;
 
 public class MealOptionsActivity extends AppCompatActivity {
 
- // int[] IMAGES = {R.drawable.cup, R.drawable.chicken, R.drawable.chicken, R.drawable.cup};
+ int[] IMAGES = {R.drawable.cup, R.drawable.chicken, R.drawable.chicken, R.drawable.cup};
 
- // String[] Names = {"Breakfast", "Lunch", "Dinner", "Snack" };
- // @Override
- // protected void onCreate(Bundle savedInstanceState) {
- //     super.onCreate(savedInstanceState);
- //     setContentView(R.layout.activity_meal_options);
- //     ListView listView =(ListView)findViewById(R.id.listview);
+ String[] Names = {"Breakfast", "Lunch", "Dinner", "Snack" };
 
- //     MealOptionsAdapter mealOptionsAdapter = new MealOptionsAdapter();
- //     listView.setAdapter(mealOptionsAdapter);
- // }
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+     super.onCreate(savedInstanceState);
+     setContentView(R.layout.activity_meal_options);
+     ListView listView =(ListView)findViewById(R.id.listview);
 
- // class MealOptionsAdapter extends BaseAdapter
- // {
+     MealOptionsAdapter mealOptionsAdapter = new MealOptionsAdapter();
+     listView.setAdapter(mealOptionsAdapter);
+     listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+         @Override
+         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
- //     @Override
- //     public int getCount() {
- //         return IMAGES.length;
- //     }
+             //Get The Item Name
+           String item = (String) adapterView.getItemAtPosition(i);
+             //Switch
 
- //     @Override
- //     public Object getItem(int i) {
- //         return null;
- //     }
+             //Starta Food List passando a Variavel que tem o nome breakfast.
+         }
+     });
+ }
 
- //     @Override
- //     public long getItemId(int i) {
- //         return 0;
- //     }
+ class MealOptionsAdapter extends BaseAdapter
+ {
 
- //     @Override
- //     public View getView(int i, View view, ViewGroup viewGroup) {
- //         view = getLayoutInflater().inflate(R.layout.meal_options_layout, null);
+     @Override
+     public int getCount() {
+         return IMAGES.length;
+     }
 
- //         ImageView imageView = (ImageView)view.findViewById(R.id.imageView);
- //         TextView textView = (TextView)view.findViewById(R.id.textView);
+     @Override
+     public Object getItem(int i) {
+         return Names[i];
+     }
 
- //         imageView.setImageResource(IMAGES[i]);
- //         textView.setText(Names[i]);
+     @Override
+     public long getItemId(int i) {
+         return 0;
+     }
 
- //         return view;
- //     }
- // }
+     @Override
+     public View getView(int i, View view, ViewGroup viewGroup) {
+
+         if (view == null)
+            view = getLayoutInflater().inflate(R.layout.meal_options_layout, null);
+
+         ImageView imageView = (ImageView)view.findViewById(R.id.imageView3);
+         TextView textView = (TextView)view.findViewById(R.id.textView_Meal);
+
+         imageView.setImageResource(IMAGES[i]);
+         textView.setText(Names[i]);
+
+         return view;
+     }
+ }
 }
 
