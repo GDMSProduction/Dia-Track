1f0a138ff534ba31358e72a6119a8bc8174fd0dd
Evelyn Lima Rocha <elimarocha@student.fullsail.edu>
2018-05-08 16:58:52 -0400

    Up and Down button

--- Diatrack/app/src/main/java/com/Diatrack/Activities/SelectedFoodActivity.java
+++ Diatrack/app/src/main/java/com/Diatrack/Activities/SelectedFoodActivity.java
@@ -1,6 +1,7 @@
 package com.Diatrack.Activities;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,42 +14,51 @@ import static com.Diatrack.R.id.txt_quantity;
 
 public class SelectedFoodActivity extends AppCompatActivity {
 
+    float quantity = 1;
+
+    TextView quantityLabel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selected__food);
         Button save = (Button) findViewById(R.id.btn_save);
+        quantityLabel = (TextView) findViewById(txt_quantity);
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(SelectedFoodActivity.this, MealDoneActivity.class));
             }
-        });}
+        });
 
+        FloatingActionButton up = findViewById(R.id.btn_up);
+        FloatingActionButton down = findViewById(R.id.btn_down);
 
-    public void button(View view){
-        switch(view.getId())
-        {
-            float numtest = 1;
-            case R.id.btn_up:
-                numtest+=1;
-                TextView t = (TextView) findViewById(txt_quantity);
-                t.setText(Integer.toString(numtest));
-                break;
-        }
+        up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addone();
+            }
+        });
+        down.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (quantity >0 )
+                subtractone();
+            }
+        });
     }
-    public void addone(View v){
-        float numtest = 1;
-        numtest += 0.25;
-        TextView quantity = (TextView) findViewById(txt_quantity);
-        quantity.setText(numtest+"");
+
+
+    public void addone(){
+        quantity += 0.25;
+        quantityLabel.setText(quantity+"");
     }
 
-    public void subtractone(View v){ ;
-        numtest += 0.25;
-        TextView quantity = (TextView) findViewById(txt_quantity);
-        quantity.setText(numtest+"");
+    public void subtractone(){
+        quantity -= 0.25;
+        quantityLabel.setText(quantity+"");
     }
     }
 
