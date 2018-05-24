41c33a094e2a1c7552a4a41cb8292c8a59f76d37
Justin Marshall <JLMarshall1@student.fullsail.edu>
2018-05-24 17:40:03 -0400

    New User

--- Diatrack/app/src/main/java/com/Diatrack/Classes/Meal.java
+++ Diatrack/app/src/main/java/com/Diatrack/Classes/Meal.java
@@ -1,14 +1,14 @@
-package com.example.marshall.Diatrack.Classes;
-
-
-import com.example.marshall.Diatrack.Enums.MealType;
-
-public class Meal {
-
-    private MealType type;
-    //private Food food;
-
-    public void Meal() {
-
-    }
-}
+//package com.example.marshall.Diatrack.Classes;
+//
+//
+////import com.example.marshall.Diatrack.Enums.MealType;
+//
+//public class Meal {
+//
+//    private MealType type;
+//    //private Food food;
+//
+//    public void Meal() {
+//
+//    }
+//}
